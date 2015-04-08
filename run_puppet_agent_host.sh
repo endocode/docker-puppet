f82f7fbce5e401e576a23c08afac026b6ddb8e69
root <root@static.213-239-209-146.clients.your-server.de>
2015-04-08 18:44:16 +0200

    Updated docker scripts

--- run_puppet_agent_host.sh
+++ run_puppet_agent_host.sh
@@ -5,15 +5,22 @@ if [ -z "$1" ]; then
 	exit 1
 fi
 
+DNSMASQ_HOSTS=/etc/hosts.dnsmasq.conf
+DNSMASQ_PID=/var/run/dnsmasq/dnsmasq.pid
 NAME=$1
-CID=$(docker run -d --name $NAME --hostname $NAME -v /etc/hosts:/etc/hosts -e PUPPETMASTER_TCP_HOST=puppet-master -e SSH_KEYS="$(cat ~/.ssh/authorized_keys)" endocode/puppet-agent)
+CID=$(docker run -d --name $NAME --hostname $NAME -e PUPPETMASTER_TCP_HOST=puppet-master -e SSH_KEYS="$(cat ~/.ssh/authorized_keys)" endocode/puppet-agent)
+
 if [ "$?" == "0" ]; then
-	IP_ADDRESS=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $CID)
-	if [[ $(egrep "$NAME$" /etc/hosts) ]]; then
-		sed -i "s/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\s\+\($NAME\)$/$IP_ADDRESS \1/g" /etc/hosts
-	else
-		echo "$IP_ADDRESS $NAME" | tee -a /etc/hosts
+	if [ ! -f $DNSMASQ_HOSTS ]; then
+		touch $DNSMASQ_HOSTS
 	fi
+	IP_ADDRESS=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $CID)
+	if [[ $(egrep "$NAME$" $DNSMASQ_HOSTS) ]]; then
+		sed -i "s/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\s\+\($NAME\)$/$IP_ADDRESS \1/g" $DNSMASQ_HOSTS
+	else
+		echo "$IP_ADDRESS $NAME" | tee -a $DNSMASQ_HOSTS
+	fi
+	kill -HUP $(cat $DNSMASQ_PID)
 else
 	echo Failed
 	exit 1
--- run_puppet_master.sh
+++ run_puppet_master.sh
@@ -1,15 +1,22 @@
 #!/bin/bash
-PREIFIX_DIR=$(pwd)
+DNSMASQ_HOSTS=/etc/hosts.dnsmasq.conf
+DNSMASQ_PID=/var/run/dnsmasq/dnsmasq.pid
+PREFIX_DIR=$(pwd)
 PUPPET_FILES="puppet_files"
 NAME="puppet-master"
-CID=$(docker run -d --name $NAME --hostname $NAME -v /etc/hosts:/etc/hosts -v $PREIFIX_DIR/$PUPPET_FILES/ssh-keys:/root/.ssh/authorized_keys.d -v $PREIFIX_DIR/$PUPPET_FILES/manifests:/etc/puppet/manifests -v $PREIFIX_DIR/$PUPPET_FILES/modules:/etc/puppet/modules -v $PREIFIX_DIR/$PUPPET_FILES/ssl-keys:/var/lib/puppet/ssl endocode/puppet-master)
+CID=$(docker run -d --name $NAME --hostname $NAME -v $PREFIX_DIR/$PUPPET_FILES/ssh-keys:/root/.ssh/authorized_keys.d -v $PREFIX_DIR/$PUPPET_FILES/manifests:/etc/puppet/manifests -v $PREFIX_DIR/$PUPPET_FILES/modules:/etc/puppet/modules -v $PREFIX_DIR/$PUPPET_FILES/ssl-keys:/var/lib/puppet/ssl endocode/puppet-master)
+
 if [ "$?" == "0" ]; then
-	IP_ADDRESS=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $CID)
-	if [[ $(egrep "$NAME$" /etc/hosts) ]]; then
-		sed -i "s/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\s\+\($NAME\)$/$IP_ADDRESS \1/g" /etc/hosts
-	else
-		echo "$IP_ADDRESS $NAME" | tee -a /etc/hosts
+	if [ ! -f $DNSMASQ_HOSTS ]; then
+		touch $DNSMASQ_HOSTS
 	fi
+	IP_ADDRESS=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" $CID)
+	if [[ $(egrep "$NAME$" $DNSMASQ_HOSTS) ]]; then
+		sed -i "s/^[0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\s\+\($NAME\)$/$IP_ADDRESS \1/g" $DNSMASQ_HOSTS
+	else
+		echo "$IP_ADDRESS $NAME" | tee -a $DNSMASQ_HOSTS
+	fi
+	kill -HUP $(cat $DNSMASQ_PID)
 else
 	echo Failed
 	exit 1
